79c2f69032cf32dba777cb873575bc9b4c889709
Fajar <kucaykojek@gmail.com>
2025-09-09 13:54:15 +0700

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,84 @@
-import Image from "next/image";
+import { cn } from "@/utils/cn";
+
+import Signature from "@/components/Signature";
+import SocialInstagram from "@/components/social/Instagram";
+import SocialSpotify from "@/components/social/Spotify";
+import SocialTikTok from "@/components/social/TikTok";
+import SocialYouTube from "@/components/social/YouTube";
+
+const platforms = [
+  {
+    label: "Watch on YouTube",
+    icon: <SocialYouTube width={30} />,
+    href: "https://www.youtube.com/@hellooonada",
+    className: "cta-youtube",
+  },
+  {
+    label: "Listen on Spotify",
+    icon: <SocialSpotify width={30} />,
+    href: "https://open.spotify.com/artist/5XUy3uAlHzD8mUGZDBE9Kw?si=pvvuqFcQTUeVZBeZ97PUZg",
+    className: "cta-spotify",
+  },
+];
+
+const socials = [
+  {
+    label: "Instagram",
+    icon: <SocialInstagram width={26} />,
+    href: "https://www.instagram.com/hellooonada",
+  },
+  {
+    label: "TikTok",
+    icon: <SocialTikTok width={26} />,
+    href: "https://www.tiktok.com/@hellooonada",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative w-screen h-screen">
+      <video src="/bg.mp4" muted autoPlay loop className="top-0 left-0 fixed w-screen h-screen object-cover" />
+      <div className="top-0 left-0 z-[1] fixed w-screen h-screen pattern" />
+      <div className="z-10 relative flex flex-col justify-end items-center gap-6 p-4 w-screen h-[calc(100dvh-10rem)] text-center">
+        <h1 className="font-bold text-2xl tracking-[0.5rem]">Nada Ayu Lestari</h1>
+        <div className="flex md:flex-row flex-col items-center gap-4 text-left">
+          {platforms.map((val, idx) => (
+            <a
+              key={`platform-${idx}`}
+              className={cn(
+                "flex justify-center items-center gap-2 bg-white/10 px-4 py-1 rounded-xl w-full md:w-auto h-12 font-medium transition-all ease-in-out",
+                val.className
+              )}
+              href={val.href}
+              target="_blank"
+            >
+              {val.icon}
+              <span aria-label={val.label}>{val.label}</span>
+            </a>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="relative space-y-2">
+          <span className="text-sm">Social Media</span>
+          <div className="flex flex-wrap justify-center items-center gap-4">
+            {socials.map((val, idx) => (
+              <a
+                key={`platform-${idx}`}
+                className={cn(
+                  "flex justify-center items-center hover:bg-white/10 rounded-lg w-10 h-10 transition-all ease-in-out"
+                )}
+                href={val.href}
+                title={val.label}
+                target="_blank"
+              >
+                {val.icon}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="bottom-4 left-1/2 z-[2] fixed w-fit -translate-x-1/2">
+        <Signature />
+      </div>
     </div>
   );
 }
